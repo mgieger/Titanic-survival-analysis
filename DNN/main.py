6425c02b1374c4e59e6518a5eed6c6033a5e2a27
Matt <mgieger@pdx.edu>
2018-03-14 22:55:24 -0700

    Init keras net.

--- DNN/main.py
+++ DNN/main.py
@@ -4,33 +4,41 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn import datasets
-from preprocessor2 import Preprocessor
+from preprocessor import Preprocessor
 
 #import keras
 from keras.models import Sequential
-from keras.layers import Dense
-
-np.random.seed(7)
+from keras.layers import Dense, Activation
+from keras import optimizers
 
 
 def main():
-    #files
-    directory = './save/{}'.format(time.ctime().replace(' ', '-'))
-    model_file = directory + '/model-set-{}'
-    results_file = directory + '/results-set-{}'
-    train_file = './data/titanic_train.csv'
+    np.random.seed(7)
+   # input_matrix = np.random.uniform(low = 0.0, high = 1.0, size = (800, 10))
+    #labels = np.random.randint(low = 0, high = 2, size = (800, 1))
+    #new_labels = labels.astype(float)
+    #dataset = np.concatenate((new_labels, input_matrix), axis = 1)
+    dataset = np.loadtxt("pima_indians.csv", delimiter = ',')
 
-    feature_set = ['pclass', 'sex', 'age', 'fare', 'sibSp', 'parch', 'cabin', 'embarked']
+    data_samples = dataset[:, 0:8]
+    data_labels = dataset[:, 8]
+    #data_labels = dataset[:, 0]
+    #data_samples = dataset[:, 1: ]
 
-    preprocessor = Preprocessor(train_file)
-    dataset = preprocessor.get_matrix()
-    # for set_num, features in enumerate(feature_set):
-    #     dataset = preprocessor.get_matrix(features + ['Survived'])
-    #     data, labels = dataset[:,:-1], dataset[:,-1]
-    #     svc = SVC(cache_size=cache_size, max_iter=max_iterations)
-    #     clf = GridSearchCV(svc, param_grid=parameters, cv=cv_size, return_train_score=True)
-    #     results = clf.fit(data, labels)
+    #create the model.
+    model = Sequential()
+    model.add(Dense(units = 12, input_dim = 8, activation='sigmoid', use_bias = True))
+    model.add(Dense(8, activation='sigmoid', use_bias = True))
+    #ADD MORE LAYERS HERE AS DESIRED#
+    model.add(Dense(1, activation='sigmoid'))
 
+    #compile the model.
+    #sgd = keras.optimizer.SGD(lr=0.01, momentum=0.9)
+    model.compile(optimizer='SGD', loss='mean_squared_error', metrics=['accuracy'])
+    model.fit(data_samples, data_labels, epochs = 1, batch_size=5)
+    predictions = model.predict(data_samples)
+    rounded = [round(x[0]) for x in predictions]
+    print(rounded)
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
