821791bf559657761b77c57befbec075bbd05c8d
Matt <mgieger@pdx.edu>
2018-03-16 16:59:34 -0700

    Initial working model.

--- DNN/main.py
+++ DNN/main.py
@@ -11,7 +11,7 @@ from sklearn import datasets
 from keras.models import Sequential
 from keras.layers import Dense, Activation
 from keras import optimizers
-
+#from keras.utils.vis_utils import plot_model
 
 def main():
     np.random.seed(7)
@@ -36,33 +36,37 @@ def main():
     preprocessor = Preprocessor(full_file)
     data = preprocessor.get_matrix_scaled(['pclass', 'name', 'sex', 'age', 'ticket', 'cabin', 'fare', 'survived'])
     
-    ###TODO:VERIFY BELOW INDEXING IS PROPER. seeing should be df[:size, : -1]... ?
     TRAIN_SIZE = math.ceil(data.shape[0]*.70)
     train_data, train_labels = data[:TRAIN_SIZE,:-1], data[:TRAIN_SIZE,-1:]
     # train, train_labels, train_pid = data[:TEST_SIZE, :-1], data[:TEST_SIZE, -2], data[:TEST_SIZE, -1]
     test_data, test_labels = data[TRAIN_SIZE:,:-1], data[TRAIN_SIZE:,-1]
     print(train_data.shape)
-    #print(data.shape)
     print(test_data.shape)
-    #print(train_labels)
-
-######DATA EXISTS in train and test sets. STILL must normalize.
 
     #create the model.
     model = Sequential()
-    model.add(Dense(units = 12, input_dim = 7, activation='sigmoid', use_bias = True))
-    model.add(Dense(8, activation='sigmoid', use_bias = True))
+    model.add(Dense(units = 12, input_dim = 7, activation='sigmoid', use_bias=True))
+    model.add(Dense(8, activation='sigmoid', use_bias=True))
     #ADD MORE LAYERS HERE AS DESIRED#
     model.add(Dense(1, activation='sigmoid'))
+    
+    #Create visualization and png model overview to verify structure.
+    print(model.summary())
+#    plot_model(model, to_file='titanic_model.png', show_shapes = True, show_layer_names = True)
 
     #compile the model.
-    #sgd = keras.optimizer.SGD(lr=0.01, momentum=0.9)
-    model.compile(optimizer='SGD', loss='mean_squared_error', metrics=['accuracy'])
-    model.fit(train_data, train_labels, epochs = 10, batch_size=5)
+    sgd = optimizers.SGD(lr=0.1, momentum=0.9)
+    model.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
+    model.fit(train_data, train_labels, epochs = 10, batch_size = 1, verbose = 2, shuffle = True)
+  
+    score = model.evaluate(test_data, test_labels, batch_size= 1)
+    print(score)
+
     ####WHY are we predicting on the training data??
-    predictions = model.predict(train_data) ####<<___TESTDATA NOT TRAIN FOOL!
+    predictions = model.predict(test_data) ####<<___TESTDATA NOT TRAIN?!?
     rounded = [round(x[0]) for x in predictions]
-    print(rounded)
+    #print(rounded)
+    
 
 if __name__ == '__main__':
     main()
