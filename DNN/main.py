9365e673416e2635ee5a652a58bf62743b8cff88
Matt <mgieger@pdx.edu>
2018-03-19 21:58:05 -0700

    Updating with dataset printouts.

--- DNN/main.py
+++ DNN/main.py
@@ -1,6 +1,7 @@
 import json
 import math
 import os
+import sys
 import time
 
 from preprocessor import Preprocessor
@@ -66,84 +67,116 @@ def main():
 
     target_names = ['Class 0: Perished', 'Class 1: Survived']
     sgd = optimizers.SGD(lr=0.05, momentum=0.9)
+    drop_rates = [0.0, 0.2, 0.3]
+    node_nums = [10]
+
+    for node in node_nums:
+    #     for nodes in node_nums:
+        ##Create the test input models.
+        np.random.seed(79)
+        
+        model1 = Sequential()
+        model1.add(Dense(units = 10, input_dim = 10, activation='relu', use_bias=True))
+        model1.add(Dropout(0.3, seed=None))
+        model1.add(Dense(units = node, input_dim = 10, activation='sigmoid', use_bias=True))
+        model1.add(Dense(1, activation='sigmoid'))
+        
+        #Create visualization overview to verify model structure.
+        print('MODEL SPECIFICATIONS:\n2 layers, \nLayer 1: 10 nodes Dropout: 0.3 ',
+                'Layer 2: Hidden nodes: ', node, ' No dropout.')
+        print(model1.summary())
+
+        #Compile the model.
+        model1.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
+        
+        #Train and Evaluate the model.
+        model1.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
+        score1 = model1.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
+        print(model1.metrics_names)
+        print(score1, '\n')
+        test_predictions1 = model1.predict_classes(test_data, batch_size = 1)
+
+        print(classification_report(test_labels, test_predictions1, target_names=target_names))
+        print(confusion_matrix(test_labels, test_predictions1)) 
+
 
     ##Create the 1 layer model.
-    np.random.seed(79)
+    # np.random.seed(79)
     
-    model1 = Sequential()
-    model1.add(Dense(units = 10, input_dim = 10, activation='sigmoid', use_bias=True))
-    model1.add(Dropout(0.30, seed=None))
-    model1.add(Dense(1, activation='sigmoid'))
+    # model1 = Sequential()
+    # model1.add(Dense(units = 10, input_dim = 10, activation='sigmoid', use_bias=True))
+    # model1.add(Dropout(0.30, seed=None))
+    # model1.add(Dense(1, activation='sigmoid'))
     
-    #Create visualization overview to verify model structure.
-    print(model1.summary())
+    # #Create visualization overview to verify model structure.
+    # print(model1.summary())
 
-    #Compile the model.
-    model1.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
+    # #Compile the model.
+    # model1.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
     
-    #Train and Evaluate the model.
-    model1.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
-    score1 = model1.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
-    print(model1.metrics_names)
-    print(score1, '\n')
-    test_predictions1 = model1.predict_classes(test_data, batch_size = 1)
+    # #Train and Evaluate the model.
+    # model1.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
+    # score1 = model1.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
+    # print(model1.metrics_names)
+    # print(score1, '\n')
+    # test_predictions1 = model1.predict_classes(test_data, batch_size = 1)
 
-    print(classification_report(test_labels, test_predictions1, target_names=target_names))
-    print(confusion_matrix(test_labels, test_predictions1))
+    # print(classification_report(test_labels, test_predictions1, target_names=target_names))
+    # print(confusion_matrix(test_labels, test_predictions1))
     
 
-    ##Create 2 layer model.
-    np.random.seed(79)
+    # ##Create 2 layer model.
+    # np.random.seed(79)
     
-    model2 = Sequential()
-    model2.add(Dense(units = 10, input_dim = 10, activation='sigmoid', use_bias=True))
-    model2.add(Dropout(0.312))
-    model2.add(Dense(4, activation='sigmoid', use_bias=True))
-    model2.add(Dense(1, activation='sigmoid'))
+    # model2 = Sequential()
+    # model2.add(Dense(units = 10, input_dim = 10, activation='sigmoid', use_bias=True))
+    # model2.add(Dropout(0.312))
+    # model2.add(Dense(4, activation='sigmoid', use_bias=True))
+    # model2.add(Dense(1, activation='sigmoid'))
     
-    #Create visualization overview to verify model structure.
-    print(model2.summary())
+    # #Create visualization overview to verify model structure.
+    # print(model2.summary())
 
-    #Compile the model.
-    model2.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
+    # #Compile the model.
+    # model2.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
     
-    #Train and Evaluate the model.
-    model2.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
-    score2 = model2.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
-    print(model2.metrics_names)
-    print(score2, '\n')
-    test_predictions2 = model2.predict_classes(test_data, batch_size = 1)
+    # #Train and Evaluate the model.
+    # model2.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
+    # score2 = model2.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
+    # print(model2.metrics_names)
+    # print(score2, '\n')
+    # test_predictions2 = model2.predict_classes(test_data, batch_size = 1)
 
-    print(classification_report(test_labels, test_predictions2, target_names=target_names))
-    print(confusion_matrix(test_labels, test_predictions2))
+    # print(classification_report(test_labels, test_predictions2, target_names=target_names))
+    # print(confusion_matrix(test_labels, test_predictions2))
 
 
     ##Create 3 layer model.
-    np.random.seed(79)
+    # np.random.seed(79)
     
-    model3 = Sequential()
-    model3.add(Dense(units = 10, input_dim = 10, activation='sigmoid', use_bias=True))
-    model3.add(Dropout(0.31075, seed=None))
-    model3.add(Dense(8, activation='sigmoid', use_bias=True))
-    model3.add(Dropout(0.2, seed=None))
-    model3.add(Dense(4, activation='sigmoid', use_bias=True))
-    model3.add(Dense(1, activation='sigmoid'))
+    # model3 = Sequential()
+    # model3.add(Dense(units = 10, input_dim = 10, activation='sigmoid', use_bias=True))
+    # model3.add(Dropout(0.31075, seed=None))
+    # model3.add(Dense(8, activation='sigmoid', use_bias=True))
+    # model3.add(Dropout(0.2, seed=None))
+    # model3.add(Dense(4, activation='sigmoid', use_bias=True))
+    # model3.add(Dense(1, activation='sigmoid'))
     
-    #Create visualization overview to verify model structure.
-    print(model3.summary())
+    # #Create visualization overview to verify model structure.
+    # print(model3.summary())
 
-    #Compile the model.
-    model3.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
+    # #Compile the model.
+    # model3.compile(optimizer = sgd, loss = 'mean_squared_error', metrics = ['accuracy'])
     
-    #Train and Evaluate the model.
-    model3.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
-    score3 = model3.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
-    print(model3.metrics_names)
-    print(score3, '\n')
-    test_predictions3 = model3.predict_classes(test_data, batch_size = 1)
+    # #Train and Evaluate the model.
+    # model3.fit(train_data, train_labels, epochs = 5, batch_size = 1, verbose = 2, shuffle = True)
+    # score3 = model3.evaluate(test_data, test_labels, batch_size = 1, verbose=0)
+    # print(model3.metrics_names)
+    # print(score3, '\n')
+    # test_predictions3 = model3.predict_classes(test_data, batch_size = 1)
 
-    print(classification_report(test_labels, test_predictions3, target_names=target_names))
-    print(confusion_matrix(test_labels, test_predictions3))
+    # print(classification_report(test_labels, test_predictions3, target_names=target_names))
+    # print(confusion_matrix(test_labels, test_predictions3))
 
 
 if __name__ == '__main__':
